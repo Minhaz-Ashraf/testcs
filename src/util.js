5d77e0292ba66e85938f9bc428d1c0bd78284fd9
Minhaz Ashraf <minhazashraf590@gmail.com>
2024-06-11 22:55:06 +0530

    changes

--- src/util.js
+++ src/util.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const apiurl = axios.create({
-    //  baseURL: "https://admincs.gauravdesign.com",
- baseURL: "http://localhost:5000",
+     baseURL: "https://admincs.gauravdesign.com",
+//  baseURL: "http://localhost:5000",
   // You can add other default configurations here if needed
 });
 
