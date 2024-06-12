a814b1b2ad123943611c5b5d8593aeada7f6db1d
Minhaz Ashraf <124189905+Minhaz-Ashraf@users.noreply.github.com>
2024-06-12 19:03:58 +0530

    Update NewJoin.jsx

--- src/user/matches/NewJoin.jsx
+++ src/user/matches/NewJoin.jsx
@@ -6,16 +6,20 @@ import { userDataStore } from "../../Stores/slices/AuthSlice";
 import apiurl from "../../util";
 import DataNotFound from "../../components/DataNotFound";
 import Skeleton from "react-loading-skeleton";
-import 'react-loading-skeleton/dist/skeleton.css'
+import "react-loading-skeleton/dist/skeleton.css";
 const NewJoin = () => {
   const { userData, userId } = useSelector(userDataStore);
   const [isLoading, setIsLoading] = useState(true);
   const [matchData, setMatchData] = useState([]);
-  
+  const [blockedUsers, setBlockedUsers] = useState([]);
+
   const fetchData = async () => {
     if (userData && userData?.gender) {
       try {
-        const partnerDetails = {...userData?.partnerPreference, gender : userData?.gender};
+        const partnerDetails = {
+          ...userData?.partnerPreference,
+          gender: userData?.gender,
+        };
         const response = await apiurl.get(`/newlyJoined/${userId}`, {
           params: partnerDetails,
         });
@@ -25,42 +29,61 @@ const NewJoin = () => {
       } finally {
         setIsLoading(false);
       }
-      }
     }
- 
+  };
 
   useEffect(() => {
     fetchData();
   }, [userData]);
 
+  const handleBlockUser = (id) => {
+    setBlockedUsers((prevBlockedUsers) => [...prevBlockedUsers, id]);
+    setMatchData((prevMatchData) =>
+      prevMatchData.filter((item) => item._id !== id)
+    );
+  };
+
+  const handleUnblockUser = (id) => {
+    setBlockedUsers((prevBlockedUsers) =>
+      prevBlockedUsers.filter((userId) => userId !== id)
+    );
+    fetchData(); // Re-fetch data to include unblocked user
+  };
+
   return (
     <>
-    <Match />
-    <div className="mb-28">
-    {isLoading ? (
-      <>
-      <div className="px-96  w-full mt-5">
-          <Skeleton height={300}  />
-        
-        </div>
-        <div className="px-96  w-full mt-5">
-          <Skeleton height={300}  />
-        
-        </div></>  // Display a loading message while data is being fetched
-    ) : matchData.length === 0 ? (
-      <DataNotFound
-        className="flex flex-col items-center ml-20"
-        message="Data not found"
-        linkText="Back to Dashboard"
-        linkDestination="/user-dashboard"
-      />
-    ) : (
-      matchData.map((item, index) => (
-        <Card item={item} key={index} fetchData={fetchData}/>
-      ))
-    )}
-    </div>
-  </>
+      <Match />
+      <div className="mb-28">
+        {isLoading ? (
+          <>
+            <div className="px-96  w-full mt-5">
+              <Skeleton height={300} />
+            </div>
+            <div className="px-96  w-full mt-5">
+              <Skeleton height={300} />
+            </div>
+          </> // Display a loading message while data is being fetched
+        ) : matchData.length === 0 ? (
+          <DataNotFound
+            className="flex flex-col items-center ml-20"
+            message="Data not found"
+            linkText="Back to Dashboard"
+            linkDestination="/user-dashboard"
+          />
+        ) : (
+          matchData.map((item, index) => (
+            <Card
+              item={item}
+              key={index}
+              fetchData={fetchData}
+              handleBlockUser={handleBlockUser}
+              handleUnblockUser={handleUnblockUser}
+              blockedUsers={blockedUsers}
+            />
+          ))
+        )}
+      </div>
+    </>
   );
 };
 
